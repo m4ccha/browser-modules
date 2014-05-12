a0e39f88c8d72f3b93ac3bee1b591107e5a1cb03
Maciej Chałapuk <maciej@chalapuk.pl>
2014-05-12 03:11:52 +0200

    + tests for bg/fg scoping

---
+++ spec/scope.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var modularApp = require("../compat/node");
+
+describe("browser-modules", function() {
+  describe("after creating application", function() {
+    var app;
+    beforeEach(function() {
+      app = modularApp.create("test");
+      expect(app).not.toBeNull();
+    });
+
+    describe("when in bg code", function() {
+      it("shouldn't be able to use closure in code posted to bg", function() {
+        var object = "local";
+        app.postBackground(function() { object = "changed"; });
+        expect(object).toBe("local");
+      });
+      it("shouldn't be able to access fg globals", function() {
+        global.test = "foreground";
+        app.postBackground(function() {
+          postForeground(function(arg) { global.fromFg = arg }, global.test);
+        });
+        expect(global.fromFg).not.toBeDefined();
+      });
+    });
+  });
+});
+
