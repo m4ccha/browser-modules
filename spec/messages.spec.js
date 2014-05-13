a1ce56278e25d54318c1887f82aa9582d07b498e
Maciej Chałapuk <maciej@chalapuk.pl>
2014-05-13 02:55:03 +0200

    + more tests for passing messages

--- spec/messages.spec.js
+++ spec/messages.spec.js
@@ -25,6 +25,14 @@ describe("browser-modules", function() {
       app.postBackground(function() {});
       app.postBackground(function() {});
     });
+    it("shouldn't be able to post not invokable objects to bg", function() {
+      expect(function() { app.postBackground({}); }).toThrow();
+      expect(function() { app.postBackground([]); }).toThrow();
+      expect(function() { app.postBackground(""); }).toThrow();
+      expect(function() { app.postBackground(0); }).toThrow();
+      expect(function() { app.postBackground(true); }).toThrow();
+      expect(function() { app.postBackground(undefined); }).toThrow();
+    });
 
     describe("when in bg code", function() {
       it("should be able to post empty function back to fg", function(done) {
@@ -42,13 +50,56 @@ describe("browser-modules", function() {
           postForeground(function() { global.fixture.tick(); });
         });
       });
-      it("should be able to post a string back to fg", function(done) {
+      it("should be able to post f with string arg back to fg", function(done) {
         global.fixture.done = function(arg) {
           expect(arg).toBe("test");
           done();
         };
         app.postBackground(function() {
           postForeground(function(arg) { global.fixture.done(arg); }, "test");
+        });
+      });
+      it("should be able to post f with int arg back to fg", function(done) {
+        global.fixture.done = function(arg) {
+          expect(arg).toBe(0xBaadF00D);
+          done();
+        };
+        app.postBackground(function() {
+          postForeground(
+            function(arg) { global.fixture.done(arg); },
+            0xBaadF00D
+            );
+        });
+      });
+      it("should be able to post f with array arg back to fg", function(done) {
+        global.fixture.done = function(arg) {
+          expect(arg).toEqual([0, 1, 2, 4, 8]);
+          done();
+        };
+        app.postBackground(function() {
+          postForeground(
+            function(arg) { global.fixture.done(arg); },
+            [0, 1, 2, 4, 8]
+            );
+        });
+      });
+      it("should be able to post f with object arg back to fg", function(done) {
+        global.fixture.done = function(arg) {
+          expect(arg).toEqual({});
+          done();
+        };
+        app.postBackground(function() {
+          postForeground(function(arg) { global.fixture.done(arg); }, {});
+        });
+      });
+      it("should be able to post f with 2 args", function(done) {
+        global.fixture.done = function(arg0, arg1) {
+          expect(arg0).toBe(0);
+          expect(arg1).toBe(1);
+          done();
+        };
+        app.postBackground(function() {
+          postForeground(function(a0,a1) { global.fixture.done(a0,a1); }, 0,1);
         });
       });
     });
