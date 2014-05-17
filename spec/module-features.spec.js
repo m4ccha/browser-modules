37085a59d1ffb668855773320249b38278b109c6
Maciej Chałapuk <maciej@chalapuk.pl>
2014-05-17 19:08:22 +0200

    + tests for features inside a module

---
+++ spec/module-features.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var modularApp = require("../compat/node");
+
+describe("browser-modules", function() {
+  describe("after creating application", function() {
+    var app;
+    beforeEach(function() {
+      app = modularApp.create("test");
+      expect(app).not.toBeNull();
+
+      global.fixture = {};
+    });
+    afterEach(function() {
+      app.terminate();
+    });
+
+    describe("when inside a module", function() {
+      function expectResultOfEvalInsideAModule(expected, func) {
+        return function(done) {
+          global.fixture.done = function(arg) {
+            expect(arg).toBe(expected);
+            done();
+          };
+          app.postBackground(function(code) {
+            var _eval = require("../spec/modules/eval");
+            var result = _eval("("+ code.toString() +")()");
+            postForeground(function(arg) { global.fixture.done(arg); }, result);
+          }, func.toString());
+        };
+      }
+
+      it("is able to require module with exports of type function",
+        expectResultOfEvalInsideAModule("function", function() {
+          return typeof require("../spec/modules/eval");
+        }));
+    });
+  });
+});
+
