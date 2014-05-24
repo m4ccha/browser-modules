6d937cf14227c18fc760f23310e2f9a346493c5c
Maciej Chałapuk <maciej@chalapuk.pl>
2014-05-24 06:07:00 +0200

    + tests for different module instances in different app instances

--- compat/browser.js
+++ compat/browser.js
@@ -1,5 +1,23 @@
 'use strict';
 
-window.require = function() { return window.modularApp; }
+window.require = function(moduleId) {
+  if (moduleId == "fs") {
+    return {
+      readFileSync : function(fileName) {
+        var url = "../"+ fileName;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, false);
+        xhr.send();
+        if (xhr.status !== 200) {
+          throw Error(xhr.status + " " + xhr.statusText + ": " + url);
+        }
+        var code = xhr.responseText;
+        return code
+      }
+    };
+  }
+  return window.modularApp;
+}
+
 window.global = window;
 
--- spec/scope.spec.js
+++ spec/scope.spec.js
@@ -3,6 +3,10 @@
 var modularApp = require("../compat/node");
 
 describe("browser-modules", function() {
+  beforeEach(function() {
+    global.fixture = {};
+  });
+
   describe("after creating application", function() {
     var app;
     beforeEach(function() {
@@ -25,6 +29,35 @@ describe("browser-modules", function() {
           postForeground(function(arg) { global.fromFg = arg }, global.test);
         });
         expect(global.fromFg).not.toBeDefined();
+      });
+
+      describe("with second application running", function() {
+        var app2;
+        beforeEach(function() {
+          app2 = modularApp.create("test2");
+          expect(app).not.toBeNull();
+        });
+        afterEach(function() {
+          app2.terminate();
+        });
+
+        it("should have different module instances in each app",function(done){
+          global.fixture.done = function(arg) {
+            expect(arg).not.toBeDefined();
+            done();
+          };
+          global.fixture.tick = function() {
+            app2.postBackground(function() {
+              var test = require("../spec/modules/eval").test;
+              postForeground(function(arg) { global.fixture.done(arg); }, test);
+            });
+          };
+          app.postBackground(function() {
+            var _eval = require("../spec/modules/eval");
+            var test = _eval("exports.test = true;");
+            postForeground(function() { global.fixture.tick(); });
+          });
+        });
       });
     });
   });
